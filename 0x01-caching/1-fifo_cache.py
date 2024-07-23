969d2a88ea8b2d44b4409ea4e588ba95d45cb3df
oladakinjoye <oladakinjoye@gmail.com>
2024-07-23 23:08:07 +0000

    update 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""Last-In First-Out caching module.
+"""First-In First-Out caching module.
 """
 from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class FIFOCache(BaseCaching):
     """Represents an object that allows storing and
-    retrieving items from a dictionary with a LIFO
+    retrieving items from a dictionary with a FIFO
     removal mechanism when the limit is reached.
     """
     def __init__(self):
@@ -22,12 +22,10 @@ class LIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", last_key)
         self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=True)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
     def get(self, key):
         """Retrieves an item by key.
